84e8a5dac4c0f21457d4297c420a4e2306361e1f
flyeee307 <58293256+flyeee307@users.noreply.github.com>
2020-06-19 16:37:29 +0800

    解決圖片展示的RWD問題

--- js/all.js
+++ js/all.js
@@ -26,12 +26,14 @@ $(document).ready(function(){
 	//   }
 	// });
 
+
+
 	// init Isotope
-	var $grid = $('.grid').isotope({
+	var $grid = $('.jq-grid').isotope({
 	  // options
 	  itemSelector: '.grid-item',
 	  layoutMode: 'masonry',
-	  // percentPosition: true,
+	  percentPosition: true,
 	  // layout mode options
 	  masonry: {
 	    columnWidth: '.grid-sizer'
@@ -46,6 +48,56 @@ $(document).ready(function(){
 	  var filterValue = $(this).attr('data-filter');
 	  $grid.isotope({ filter: filterValue });
 	});
+
+
+
+
+	// 正方形縮圖排版
+	var $grid = $('.jq-sqare-grid').isotope({
+	  itemSelector: '.sqare-grid-item',
+	  layoutMode: 'masonryHorizontal',
+	  masonryHorizontal: {
+	    // columnWidth: '.sqare-grid-item',
+	    rowHeight: 100,
+		gutter: 10
+	  }
+	});
+
+	// $grid.on( 'click', '.grid-item-content', function() {
+	//   $( this ).parent('.sqare-grid-item').toggleClass('is-expanded');
+	//   $grid.isotope('layout');
+	// });
+
+	// layout Isotope after each image loads
+	$grid.imagesLoaded().progress( function() {
+	  $grid.isotope('layout');
+	});
+
+	// filter items on button click
+	$('.filter-button-group').on( 'click', 'button', function() {
+	  var filterValue = $(this).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
+	});
+
+
+
+	// RWD顯示設定
+
+	function windowSize() {
+	    if ($(window).width() < 576) {
+	        $('.jq-sqare-grid').show();
+	        $('.jq-grid').hide();
+	    } else {
+	        $('.jq-sqare-grid').hide();
+	        $('.jq-grid').show();
+	    }
+	}
+	$(window).resize(function() {
+	    windowSize();
+	});
+	windowSize();
+
+
 
 	// fancybox 設定
 	$('[data-fancybox="gallery"]').fancybox({
@@ -69,19 +121,53 @@ $(document).ready(function(){
 		protect: true
 	});
 
-	// Check if current image dimensions are smaller than actual
-	instance.isScaledDown();
+	// // Check if current image dimensions are smaller than actual
+	// instance.isScaledDown();
 
-	// Scale image to the actual size of the image
-	instance.scaleToActual( x, y, duration );
+	// // Scale image to the actual size of the image
+	// instance.scaleToActual( x, y, duration );
 
-	// Check if image dimensions exceed parent element
-	instance.canPan();
+	// // Check if image dimensions exceed parent element
+	// instance.canPan();
 
-	// Scale image to fit inside parent element
-	instance.scaleToFit( duration );
+	// // Scale image to fit inside parent element
+	// instance.scaleToFit( duration );
+
+
+	// hover圖片時其他隱藏
+	// $('.grid-item').hover(function() {
+	// 	$(this).siblings().stop().fadeOut('400');
+	// 	}, function() {
+	// 	$(this).siblings().stop().fadeIn('400');
+	// });
 
 
 
+
+	// UI Scroll 上下捲動，控制 Header 的消失與顯示
+	var lastScrollY = 0;
+
+	$(window).scroll(function(event) {
+		
+		var st = this.scrollY;
+
+		if (st < lastScrollY) {
+			$('.header').removeClass('hideUp');
+		} else {
+			$('.header').addClass('hideUp');
+		}
+		lastScrollY = st;
+	});
+
+	// 回到最頂端的TOP按鈕
+	$('.top a').click(function(event) {
+		event.preventDefault();
+		$('html,body').animate({
+			scrollTop: 0
+		},1000);
+	});
+		
 
 });
+
+
