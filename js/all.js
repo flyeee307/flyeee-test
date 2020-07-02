7da4473b0a5a443ca13b989fd777ae297df72e9a
flyeee307 <58293256+flyeee307@users.noreply.github.com>
2020-07-02 15:36:40 +0800

    首次上傳個人作品集

--- js/all.js
+++ js/all.js
@@ -1,17 +1,251 @@
 $(document).ready(function(){
-	// Filterizr 圖片篩選插件
-	$('.shouceren').filterizr();
-	$('.nav-1 li,.nav-3 li,.nav-5 li').on('click', function() {
-		$(this).toggleClass('active').siblings().removeClass('active');
+
+
+
+	// UI Scroll 上下捲動，控制 Header 的消失與顯示
+	// var lastScrollY = 0;
+
+	// $(window).scroll(function(event) {
+		
+	// 	var st = this.scrollY;
+
+	// 	if (st < lastScrollY) {
+	// 		$('.main-header').removeClass('hideUp');
+	// 	} else {
+	// 		$('.main-header').addClass('hideUp');
+	// 	}
+	// 	lastScrollY = st;
+	// });
+
+
+
+	
+
+	function headerHide() {
+		
+		var lastScrollY = 0;
+
+		$(window).scroll(function(event) {
+
+			var st = this.scrollY;
+
+			if (st < lastScrollY) {
+				$('.main-header').removeClass('hideUp');
+			} else {
+				$('.main-header').addClass('hideUp');
+			}
+			lastScrollY = st;
+		});	
+	}
+
+	if($(window).innerWidth() > 375) {
+		headerHide();
+	}
+
+	$(window).resize(function() {
+		// console.log($(window).innerWidth());
+		if($(window).innerWidth() > 375) {
+			headerHide();
+		}
 	});
-	$('.nav-2 li').on('click', function() {
-		$(this).toggleClass('active');
+
+	
+
+
+
+	
+	
+
+	
+	
+
+
+	
+
+	
+	
+	
+	
+
+
+	
+
+	/*go top*/
+	// // hide #back-top first
+	// $(".back-top").hide();
+	
+	// // fade in .back-top
+	// $(window).scroll(function () {
+	// 	if ($(this).scrollTop() > 100) {
+	// 		$('.back-top').fadeIn('100');
+	// 	} else {
+	// 		$('.back-top').fadeOut('100');
+	// 	}
+	// });
+
+	// // scroll body to 0px on click
+	// $('.back-top').click(function (bt) {
+	// 	bt.preventDefault();
+	// 	$('body,html').animate({
+	// 		scrollTop: 0
+	// 	}, 800);
+	// });
+
+
+	/* go top進階寫法 */
+	// hide #back-top first
+	$(".back-top").hide();
+
+    $(window).scroll(function(){
+		var pos = $(window).scrollTop();
+		
+		if($(window).innerWidth()<=1440){
+			if (pos > 200) {
+				$('.back-top').show(200);
+			}else{
+				$('.back-top').hide(200);
+			}
+		}
 	});
-	$('.shouceren5').filterizr({
-		filter: 1
+	
+    $(".back-top").click(function(e){
+    	e.preventDefault();
+        $("html,body").stop().animate({scrollTop:0},500);	
+    });
+    
+
+
+
+
+
+
+
+	// 點選按鈕跳到該部位
+	$('.main-hero__content .btn').click(function (event) {
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: $('.about-content').offset().top-30
+		}, 800);
+	});
+
+	// 點選nav選單跳到該部位
+	$('#home').click(function (event) {
+		event.preventDefault();
+		$('.portfolio-content').removeClass('show');
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800);
+	});
+
+	$('#about').click(function (event) {
+		event.preventDefault();
+		$('.portfolio-content').removeClass('show');
+		$('body,html').animate({
+			scrollTop: $('.about-content').offset().top-30
+		}, 800);
+	});
+
+	$('#works').click(function (event) {
+		event.preventDefault();
+		$('.portfolio-content').removeClass('show');
+		$('body,html').animate({
+			scrollTop: $('.works-content').offset().top-40
+		}, 800);
+	});
+
+	$('#contact').click(function (event) {
+		event.preventDefault();
+		$('.portfolio-content').removeClass('show');
+		$('body,html').animate({
+			scrollTop: $('.contact-content').offset().top-40
+		}, 800);
 	});
 
 
+	// 點選popup-menu__item跳到該部位
+	$('.popup-about').click(function (event) {
+		event.preventDefault();
+		$('.popup-menu').removeClass('show');
+		$('.portfolio-content').removeClass('show');
+		$('.footer').removeClass('z-top');
+		$('body,html').animate({
+			scrollTop: $('.about-content').offset().top-30
+		}, 800);
+	});
+
+	$('.popup-skills').click(function (event) {
+		event.preventDefault();
+		$('.popup-menu').removeClass('show');
+		$('.portfolio-content').removeClass('show');
+		$('.footer').removeClass('z-top');
+		$('body,html').animate({
+			scrollTop: $('.skills-content').offset().top-30
+		}, 800);
+	});
+
+	$('.popup-experience').click(function (event) {
+		event.preventDefault();
+		$('.popup-menu').removeClass('show');
+		$('.portfolio-content').removeClass('show');
+		$('.footer').removeClass('z-top');
+		$('body,html').animate({
+			scrollTop: $('.experience-content').offset().top-30
+		}, 800);
+	});
+
+	$('.popup-works').click(function (event) {
+		event.preventDefault();
+		$('.popup-menu').removeClass('show');
+		$('.portfolio-content').removeClass('show');
+		$('.footer').removeClass('z-top');
+		$('body,html').animate({
+			scrollTop: $('.works-content').offset().top-30
+		}, 800);
+	});
+
+	$('.popup-contact').click(function (event) {
+		event.preventDefault();
+		$('.popup-menu').removeClass('show');
+		$('.portfolio-content').removeClass('show');
+		$('.footer').removeClass('z-top');
+		$('body,html').animate({
+			scrollTop: $('.contact-content').offset().top-30
+		}, 800);
+	});
+
+	$('.menu-toggler').click(function(event){
+		event.preventDefault();
+		$('.popup-menu').addClass('show');
+	});
+
+	$('.popup-menu--close').click(function(event){
+		event.preventDefault();
+		$('.popup-menu').removeClass('show');
+	});
+
+
+
+	// 點選 更多作品 展開作品集
+	$('.works-group .btn').click(function(event) {
+		event.preventDefault();
+		$('.portfolio-content').addClass('show');
+		$('body,html').animate({
+			scrollTop: 0
+		}, 400);
+		$('.footer').addClass('z-top');
+		$('.footer__logo').hide(100);
+	});
+
+
+
+	
+
+
+
+	
+
+
+	
 
 	// Isotope 圖片篩選插件
 	// $('.grid').isotope({
@@ -25,15 +259,6 @@ $(document).ready(function(){
 	//     fitWidth: true
 	//   }
 	// });
-
-
-
-	
-
-
-
-
-	
 
 
 
@@ -146,28 +371,7 @@ $(document).ready(function(){
 
 
 
-	// UI Scroll 上下捲動，控制 Header 的消失與顯示
-	var lastScrollY = 0;
-
-	$(window).scroll(function(event) {
-		
-		var st = this.scrollY;
-
-		if (st < lastScrollY) {
-			$('.header').removeClass('hideUp');
-		} else {
-			$('.header').addClass('hideUp');
-		}
-		lastScrollY = st;
-	});
-
-	// 回到最頂端的TOP按鈕
-	$('.top a').click(function(event) {
-		event.preventDefault();
-		$('html,body').animate({
-			scrollTop: 0
-		},1000);
-	});
+	
 		
 
 });
